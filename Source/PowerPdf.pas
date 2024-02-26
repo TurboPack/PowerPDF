02b7d22bbb8b435f31f43c39fe056eba1acc18e6
Roman Kassebaum <roman@kassebaum.eu>
2024-02-26 23:39:09 +0100

    Copied the register code to this unit.

--- Source/PowerPdf.pas
+++ Source/PowerPdf.pas
@@ -24,16 +24,15 @@ unit PowerPdf;
 interface
 
 uses
-  System.Classes,
-  PReport,
-  PRJpegImage,
-  PRAnnotation;
+  System.Classes, PReport, PRJpegImage, PRAnnotation, PdfFonts, PdfImages,
+  PdfJpegImage;
 
 procedure Register;
+procedure Unregister;
 
 implementation
 
-{ Registoer }
+{ Register }
 
 procedure Register;
 begin
@@ -49,6 +48,54 @@ begin
                                   TPRJpegImage,
                                   TPRAnnotation
                                  ]);
+
+  RegisterClassAlias(TPdfFixedWidth, 'FixedWidth');
+  RegisterClassAlias(TPdfFixedWidthBold, 'FixedWidth-Bold');
+  RegisterClassAlias(TPdfFixedWidthBoldItalic, 'FixedWidth-BoldItalic');
+  RegisterClassAlias(TPdfFixedWidthItalic, 'FixedWidth-Italic');
+  RegisterClassAlias(TPdfArial, 'Arial');
+  RegisterClassAlias(TPdfArialBold, 'Arial-Bold');
+  RegisterClassAlias(TPdfArialBoldItalic, 'Arial-BoldItalic');
+  RegisterClassAlias(TPdfArialItalic, 'Arial-Italic');
+  RegisterClassAlias(TPdfTimesRoman, 'Times-Roman');
+  RegisterClassAlias(TPdfTimesBold, 'Times-Bold');
+  RegisterClassAlias(TPdfTimesItalic, 'Times-Italic');
+  RegisterClassAlias(TPdfTimesBoldItalic, 'Times-BoldItalic');
+//  RegisterClassAlias(TPdfScript, 'Script');
+//  RegisterClassAlias(TPdfSymbol, 'Symbol');
+
+  RegisterClassAlias(TPdfBitmapImage, 'Pdf-Bitmap');
+
+  RegisterClassAlias(TPdfJpegImage, 'Pdf-Jpeg');
+
 end;
+
+procedure Unregister;
+begin
+  UnRegisterClass(TPdfFixedWidth);
+  UnRegisterClass(TPdfFixedWidthBold);
+  UnRegisterClass(TPdfFixedWidthBoldItalic);
+  UnRegisterClass(TPdfFixedWidthBold);
+  UnRegisterClass(TPdfArial);
+  UnRegisterClass(TPdfArialBold);
+  UnRegisterClass(TPdfArialBoldItalic);
+  UnRegisterClass(TPdfArialBold);
+  UnRegisterClass(TPdfTimesRoman);
+  UnRegisterClass(TPdfTimesBold);
+  UnRegisterClass(TPdfTimesItalic);
+  UnRegisterClass(TPdfTimesBoldItalic);
+//  UnRegisterClass(TPdfScript);
+//  UnRegisterClass(TPdfSymbol);
+
+  UnRegisterClass(TPdfBitmapImage);
+
+  UnRegisterClass(TPdfJpegImage);
+
+end;
+
+initialization
+
+finalization
+  Unregister;
 
 end.
